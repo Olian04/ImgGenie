da13247846e00747b0a1104ebfb2289a5f1363d2
Oliver Anteros <rift95@live.se>
2017-05-19 00:21:25 +0200

    Fresh start

---
+++ src/injects/injects.js
@@ -0,0 +1,14 @@
+console.log("Code injected");
+
+//content script
+var clickedEl = null;
+
+document.addEventListener("contextmenu", function(event){
+    clickedEl = event.target;
+}, true);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if(request == "getClickedEl") {
+        sendResponse(clickedEl);
+    }
+});
